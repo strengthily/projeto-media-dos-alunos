4cfa5bb2e3d355fe636b954b70f1131fc185524c
strengthily <00001115499105sp@educacao.al.sp.gov.br>
2024-11-27 09:43:23 -0300

    media alunos

---
+++ media.py
@@ -0,0 +1,6 @@
+nota1 = float(input("Escreva a primeira nota: "))
+nota2 = float(input("Escreva a segunda nota:"))
+nota3 = float(input("Escreva a terceira nota:"))
+
+media = (nota1 + nota2 + nota3) / 3
+print('a media é:', media)
